6801317e778aca0eae81f8135de919e4e485abae
8937753 CANADA INC - Andre St-Louis <as87321234@gmail.com>
2024-08-08 20:45:55 -0400

    jUNIT Stuff

---
+++ src/main/java/ca/mgis/tms2flow/Tms2flowApplication.java
@@ -0,0 +1,13 @@
+package ca.mgis.tms2flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tms2flowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tms2flowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/mgis/tms2flow/Tms2flowApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mgis.tms2flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tms2flowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
