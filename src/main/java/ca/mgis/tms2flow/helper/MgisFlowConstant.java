b3974110f597309be7b03747545a9902fc89fde2
8937753 CANADA INC - Andre St-Louis <as87321234@gmail.com>
2024-08-17 14:01:21 -0400

    Create constant class

---
+++ src/main/java/ca/mgis/tms2flow/helper/MgisFlowConstant.java
@@ -0,0 +1,9 @@
+package ca.mgis.tms2flow.helper;
+
+public class MgisFlowConstant {
+
+    public static final String ID_ENROLMENT_PROCESSING_START = "id-enrolment-processing-start";
+    public static final String ID_ENROLMENT_PROCESSING_ACKT_RECEIVE_SERVICE_TASK = "id-enrolment-processing-ackt-receive-service-task";
+    public static final String ID_ENROLMENT_PROCESSING_SRE_RECEIVE_SERVICE_TASK = "id-enrolment-processing-sre-receive-service-task";
+    public static final String ID_ENROLMENT_PROCESSING_ERRT_RECEIVE_SERVICE_TASK = "id-enrolment-processing-errt-receive-service-task";
+}
