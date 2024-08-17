eb5ccf508ef09e6d33c08b17e3311f3f0fae1cb0
8937753 CANADA INC - Andre St-Louis <as87321234@gmail.com>
2024-08-17 14:03:44 -0400

    Set SpringBootApplication annotation on the application

--- src/main/java/ca/mgis/tms2flow/Tms2flowApplication.java
+++ src/main/java/ca/mgis/tms2flow/Tms2flowApplication.java
@@ -2,6 +2,7 @@ package ca.mgis.tms2flow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class Tms2flowApplication {
