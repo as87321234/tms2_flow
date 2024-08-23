ac1d274eace00fb9c150de5020192c2d76e03dc7
8937753 CANADA INC - Andre St-Louis <as87321234@gmail.com>
2024-08-22 23:15:35 -0400

    Implement Nex Value for TCN and DCN

--- src/main/java/ca/mgis/tms2flow/repository/DcnRepository.java
+++ src/main/java/ca/mgis/tms2flow/repository/DcnRepository.java
@@ -3,12 +3,15 @@ package ca.mgis.tms2flow.repository;
 
 import ca.mgis.tms2flow.model.Dcn;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
 
 @SuppressWarnings("unused")
 @Repository
 public interface DcnRepository extends JpaRepository<Dcn, Long> {
 	
-	void getNewDcn();
-	
+	@Query(value = "SELECT SCHEMA.SEQUENCE_DCN.nextval FROM dual", nativeQuery = true)
+	public BigDecimal getNextValDcn();
 }
--- src/main/java/ca/mgis/tms2flow/repository/TcnRepository.java
+++ src/main/java/ca/mgis/tms2flow/repository/TcnRepository.java
@@ -1,14 +1,18 @@
 package ca.mgis.tms2flow.repository;
 
 
-import ca.mgis.tms2flow.model.Dcn;
 import ca.mgis.tms2flow.model.Tcn;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
 
 @SuppressWarnings("unused")
 @Repository
 public interface TcnRepository extends JpaRepository<Tcn, Long> {
 	
-	Tcn getNewTcn();
+	@Query(value = "SELECT SCHEMA.SEQUENCE_TCN.nextval FROM dual", nativeQuery = true)
+	public BigDecimal getNextValTcn();
+	
 }
