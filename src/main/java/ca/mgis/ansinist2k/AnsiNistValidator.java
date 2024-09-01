933ed22df3fd8746309ce8a0ff20c81824706201
8937753 CANADA INC - Andre St-Louis <as87321234@gmail.com>
2024-09-01 08:20:57 -0400

    Add method to AnsiNistValidador abstract class

--- src/main/java/ca/mgis/ansinist2k/AnsiNistValidator.java
+++ src/main/java/ca/mgis/ansinist2k/AnsiNistValidator.java
@@ -9,7 +9,23 @@ import lombok.Setter;
 
 public abstract class AnsiNistValidator {
 	
-	public abstract void validate();
+	static public String alpha_regx;
+	static public String numeric_regx;
+	static public String special_regx;
+	static public String crlf_regx;
+	static public String period_regx;
+	
+	
+	public static String validation_1_7_7f = "/Users/stlouisa/IdeaProjects/tms2_flow/src/main/resources/validation/validation-1_7_7F.json";
+	
+	public abstract boolean validate(String tag, Integer fieldIdKey, Integer subfieldIdKey, Integer itemIdKEy, String value);
+	
+	public abstract boolean validateCharacterSet(String tag, Integer fieldIdKey, Integer subfieldIdKey, Integer itemIdKEy, String value);
+	
+	public abstract boolean validateFieldLength(String tag, Integer fieldIdKey, Integer subfieldIdKey, Integer itemIdKey, String value);
+	
+	public abstract boolean validateCondition(AnsiNistPacket packet, String tag, Integer fieldIdKey, Integer subfieldIdKey, Integer itemIdKey, String value);
+	
+	public abstract boolean validateOccurrence(AnsiNistPacket packet, String tag, Integer fieldIdKey, Integer subfieldIdKey, Integer itemIdKey, String value);
 	
 }
-	
